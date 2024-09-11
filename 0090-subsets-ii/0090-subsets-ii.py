5237fdd9a497a67640e81ca42d9c812e1656b0c8
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-11 09:41:50 -0400

    Time: 32 ms (92.16%), Space: 16.8 MB (11.84%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -5,18 +5,15 @@ class Solution:
         nums.sort()
 
         def backtrack(i, subset):
-            # this is how we know we have gone through the entire array 
             if i == len(nums):
                 res.append(subset[:])
-                return 
+                return res 
 
-            # All subsets that include nums[i]
             subset.append(nums[i])
             backtrack(i+1, subset)
             subset.pop()
-
-            # All subsets that don't include nums[i]
-            while i+1 < len(nums) and nums[i] == nums[i+1]:
+        
+            while i + 1 < len(nums) and nums[i] == nums[i+1]:
                 i += 1
             backtrack(i+1, subset)
 
