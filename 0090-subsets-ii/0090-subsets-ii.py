f0fbad09c0eaab4702a1bc58986d79b915dac1fa
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-11 10:07:13 -0400

    Time: 40 ms (52.83%), Space: 16.8 MB (46.54%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -8,17 +8,19 @@ class Solution:
             if i == len(nums):
                 res.append(subset[:])
                 return res 
-
+            
             subset.append(nums[i])
             backtrack(i+1, subset)
             subset.pop()
-        
+
             while i + 1 < len(nums) and nums[i] == nums[i+1]:
                 i += 1
             backtrack(i+1, subset)
-
+        
         backtrack(0, [])
         return res 
+
+
 
 
 
