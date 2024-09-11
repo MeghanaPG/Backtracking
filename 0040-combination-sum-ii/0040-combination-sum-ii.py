a82cc5f5ec42072e890db43ca815c18f0af121ed
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-11 10:34:44 -0400

    Time: 53 ms (72.56%), Space: 16.7 MB (34.54%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,25 +1,23 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        #Time Complexity: O(2^n)
         candidates.sort()
         res = []
-         
-        
-        #here target will be the target we are planning to sum upto because every time we add the target will change 
+
         def backtrack(cur, pos, target):
             if target == 0:
-                res.append(cur.copy()) #copy() bcz cur is going to change every time 
+                res.append(cur.copy())
             if target <= 0:
                 return 
-            
+
             prev = -1
             for i in range(pos, len(candidates)):
                 if candidates[i] == prev:
                     continue
                 cur.append(candidates[i])
-                backtrack(cur, i+1, target-candidates[i])
+                backtrack(cur, i+1, target - candidates[i])
                 cur.pop()
                 prev = candidates[i]
-        backtrack([], 0, target) 
-        return res
-    
+        backtrack([], 0, target)
+        return res 
+
+
